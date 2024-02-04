8402ed17891e216835c3e1cd3ca6c47a47a04777
mohammad5305 <mohammad.bajalal546@gmail.com>
2024-02-04 17:08:06 +0330

    fix: remove useless mapfile

--- crawl_news.sh
+++ crawl_news.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -eo pipefail
+set -xe
 
 OLD_FEED=./current.txt
 CURRENT_FEED=$(mktemp -p /tmp crawl_feed.XXX)
@@ -13,8 +13,6 @@ SHORT_COMMITS=$(mktemp -p /tmp crawl.XXXX)
 wget https://github.com/crawl/crawl/commits/master.atom -O /tmp/master.atom
 
 xmlstarlet sel -N x=http://www.w3.org/2005/Atom -t -m "//x:entry" -v "x:content"  -v "x:link/@href" -n -o "----" -n /tmp/master.atom | sed -e 's/&amp;\|&#38;/\&/g' -e 's/&lt;\|&#60;/</g' -e 's/&gt;\|&#62;/>/g' -e 's/&quot;\|&#34;/"/g' -e "s/&apos;\|&#39;/'/g" -e 's/&nbsp;|&#160;/ /g' | grep -v '^$' | sed 's/^[ \t]*//' > $CURRENT_FEED
-
-function join_by { local IFS="$1"; shift; echo "$*"; }
 
 push_news() {
     DATA=$(echo "$1" | grep -v "^\s*$")
@@ -34,14 +32,13 @@ export SHORT_COMMITS URL CHANNEL_ID
 
 if [ -f "$OLD_FEED" ]
 then
-    diff $OLD_FEED $CURRENT_FEED --changed-group-format="%>" --unchanged-group-format="" | tac -s $'\n----' |   sed -e 's/----/\x0/' -e 's/<[^>]\+>//g' | xargs -0 -I{} -- bash -c 'push_news "$@"' _ {}
+    diff $OLD_FEED $CURRENT_FEED --changed-group-format="%>" --unchanged-group-format="" | tac -s $'\n----' | sed -e 's/----/\x0/' -e 's/<[^>]\+>//g' | xargs -0 -I{} -- bash -c 'push_news "$@"' _ {}
 else
     tac -s $'\n----' $CURRENT_FEED | sed -e 's/----/\x0/' -e 's/<[^>]\+>//g' | xargs -0 -I{} -- bash -c 'push_news "$@"' _ {}
 fi
 
 if [ -s "$SHORT_COMMITS" ]; then
-    mapfile LINKS < $SHORT_COMMITS
-    curl -s -X POST $URL -d chat_id=$CHANNEL_ID -d parse_mode="HTML" -d text="<b>minor changes:</b>"$'\n'"$(join_by $'\n' "${LINKS[@]}")"
+    curl -s -X POST $URL -d chat_id=$CHANNEL_ID -d parse_mode="HTML" -d text="<b>minor changes:</b>"$'\n'"$(cat $SHORT_COMMITS)"
 fi
 
 mv "$CURRENT_FEED" "$OLD_FEED"

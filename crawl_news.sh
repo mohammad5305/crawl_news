b3e6957cd478a2e35f417a42becdb28f332bdf07
mohammad5305 <mohammad.bajalal546@gmail.com>
2024-02-04 15:47:04 +0330

    feat: send one line commits on a message

--- crawl_news.sh
+++ crawl_news.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OLD_FEED=/tmp/test.txt
+OLD_FEED=./current.txt
 CURRENT_FEED=$(mktemp -p /tmp crawl_feed.XXX)
 BOT_TOKEN=${BOT_TOKEN:=""}
 CHANNEL_ID=${CHANNEL_ID:="@dcss_news"}
@@ -11,6 +11,8 @@ SHORT_COMMITS=$(mktemp -p /tmp crawl.XXXX)
 wget https://github.com/crawl/crawl/commits/master.atom -O /tmp/master.atom
 
 xmlstarlet sel -N x=http://www.w3.org/2005/Atom -t -m "//x:entry" -v "x:content"  -v "x:link/@href" -n -o "----" -n /tmp/master.atom | sed -e 's/&amp;\|&#38;/\&/g' -e 's/&lt;\|&#60;/</g' -e 's/&gt;\|&#62;/>/g' -e 's/&quot;\|&#34;/"/g' -e "s/&apos;\|&#39;/'/g" -e 's/&nbsp;|&#160;/ /g' | grep -v '^$' | sed 's/^[ \t]*//' > $CURRENT_FEED
+
+function join_by { local IFS="$1"; shift; echo "$*"; }
 
 push_news() {
     DATA=$(echo "$1" | grep -v "^\s*$")
@@ -36,7 +38,7 @@ else
     sed -e 's/----/\x0/' -e 's/<[^>]\+>//g' $CURRENT_FEED | xargs -0 -I{} -- bash -c 'push_news "$@"' _ {}
 fi
 
-
-while read -r COMMIT; do
-    curl -s -X POST $URL -d chat_id=$CHANNEL_ID -d parse_mode="HTML" -d text="$COMMIT"
-done < $SHORT_COMMITS
+if [ -s "$SHORT_COMMITS" ]; then
+    mapfile LINKS < $SHORT_COMMITS
+    curl -s -X POST $URL -d chat_id=$CHANNEL_ID -d parse_mode="HTML" -d text="$(join_by $'\n' "${LINKS[@]}")"
+fi
